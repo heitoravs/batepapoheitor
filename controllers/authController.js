f3053f6582307fc070cb08229e54e5d9d1b9ebd9
heitoravs <alvesheitor512@gmail.com>
2024-12-10 20:52:35 -0300

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 
-// Determina o caminho do arquivo de usuários
+
 const usuariosPath =
   process.env.NODE_ENV === "production"
     ? path.join("/tmp", "usuarios.json")
     : path.join(__dirname, "../data/usuarios.json");
 
-// Função de login
+
 const login = (req, res) => {
   const { email, senha } = req.body;
 
@@ -16,7 +16,7 @@ const login = (req, res) => {
     return res.render("login", { error: "E-mail e senha são obrigatórios!" });
   }
 
-  // Carrega os usuários do arquivo JSON
+ 
   let usuarios = [];
   try {
     usuarios = JSON.parse(fs.readFileSync(usuariosPath, "utf-8"));
@@ -34,13 +34,13 @@ const login = (req, res) => {
     return res.render("login", { error: "E-mail ou senha inválidos!" });
   }
 
-  // Configura a sessão e redireciona para o menu
+ 
   req.session.usuario = usuario.nickname; // Salva o nickname na sessão
   res.cookie("ultimoAcesso", new Date().toLocaleString());
   res.redirect("/menu");
 };
 
-// Função de logout
+
 const logout = (req, res) => {
   req.session.destroy(() => {
     res.clearCookie("connect.sid");
